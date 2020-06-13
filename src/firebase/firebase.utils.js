cb3de94594d7cbb136c82cbacc497835478bd633
divesh9205 <divesh9205@gmail.com>
2020-06-14 03:08:43 +0530

    users stored in database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,33 @@ const config = {
     appId: "1:598049506233:web:4f69b89eb7b11570112c68"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+      const { displayName, email} = userAuth;
+      const createdAt = new Date();
+      
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch(error) {
+          console.log('error creating user', error.message);
+      }
+    }
+
+    return userRef;
+
+  };
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
