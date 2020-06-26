290031edd3d727e3a9186e508115423762f26997
divesh9205 <divesh9205@gmail.com>
2020-06-27 00:50:11 +0530

    stripe payment Button

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStripe = price * 100;
+    // const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51GyMIiGhQjOsTon4UKjUr7sqvRWa384ZfxFxRV6k8adjvbaE1pEHucSTc0HnvJtCucbZlhXHQK4Azivp9oW5blJs00DZFNKa4q';
 
     const onToken = token => {
